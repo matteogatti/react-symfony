013bd1a0b0d66dac7d84e049dd75421b6e535b65
Matteo Gatti <matteogatti.home@gmail.com>
2017-03-09 11:44:59 +0100

    js compilato

--- src/AppBundle/Resources/public/todolist.js
+++ src/AppBundle/Resources/public/todolist.js
@@ -4,45 +4,61 @@
 
 console.clear();
 
-const TodoForm = ({addTodo}) => {
+const TodoForm = ({ addTodo }) => {
     let input;
 
-    return (
-        <div className="col col-md-4">
-            <div className="form-group">
-                <input className="form-control" type="text" placeholder="Cosa vuoi ricordare?" ref={ node => { input = node; } } />
-                <button className="btn btn-link" onClick={ () => { addTodo(input.value); input.value = ''; } }>
-                    <i className="fa fa-plus" aria-hidden="true"></i>
-                </button>
-            </div>
-        </div>
+    return React.createElement(
+        "div",
+        { className: "col col-md-4" },
+        React.createElement(
+            "div",
+            { className: "form-group" },
+            React.createElement("input", { className: "form-control", type: "text", placeholder: "Cosa vuoi ricordare?", ref: node => {
+                    input = node;
+                } }),
+            React.createElement(
+                "button",
+                { className: "btn btn-link", onClick: () => {
+                        addTodo(input.value);input.value = '';
+                    } },
+                React.createElement("i", { className: "fa fa-plus", "aria-hidden": "true" })
+            )
+        )
     );
 };
 
-const Todo = ({todo, remove}) => {
-    return (
-        <li className="list-group-item" onClick={() => {remove(todo.id)}}>
-            {todo.todo}
-            <span className="badge">
-                <i className="fa fa-times" aria-hidden="true"></i>
-            </span>
-        </li>
+const Todo = ({ todo, remove }) => {
+    return React.createElement(
+        "li",
+        { className: "list-group-item", onClick: () => {
+                remove(todo.id);
+            } },
+        todo.todo,
+        React.createElement(
+            "span",
+            { className: "badge" },
+            React.createElement("i", { className: "fa fa-times", "aria-hidden": "true" })
+        )
     );
 };
 
-const TodoList = ({todos, remove}) => {
-    const todoNode = todos.map((todo) => {
-        return (<Todo todo={todo} key={todo.id} remove={remove}/>)
+const TodoList = ({ todos, remove }) => {
+    const todoNode = todos.map(todo => {
+        return React.createElement(Todo, { todo: todo, key: todo.id, remove: remove });
     });
-    return (
-        <div className="col col-md-4">
-            <ul className="list-group">{todoNode}</ul>
-        </div>
+    return React.createElement(
+        "div",
+        { className: "col col-md-4" },
+        React.createElement(
+            "ul",
+            { className: "list-group" },
+            todoNode
+        )
     );
 };
 
 class TodoApp extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -53,33 +69,33 @@ class TodoApp extends React.Component {
     }
 
     getTodoList() {
-        axios.get('/xhr/list').then((res) => {
+        axios.get('/xhr/list').then(res => {
             this.state.data = res.data;
             this.setState({ data: this.state.data });
-        })
+        });
     }
 
     addTodo(val) {
         axios({
             url: '/xhr/add',
             method: 'POST',
-            headers: {'X-Requested-With': 'XMLHttpRequest'},
+            headers: { 'X-Requested-With': 'XMLHttpRequest' },
             data: {
                 todo: val
             }
-        }).then((res) => {
+        }).then(res => {
             if (res.data.status) {
                 const todo = { todo: val, id: res.data.result.id };
 
                 this.state.data.push(todo);
                 this.setState({ data: this.state.data });
             }
-        })
+        });
     }
 
     deleteTodo(id) {
-        const remainder = this.state.data.filter((todo) => {
-            if(todo.id !== id) {
+        const remainder = this.state.data.filter(todo => {
+            if (todo.id !== id) {
                 return todo;
             }
         });
@@ -87,22 +103,23 @@ class TodoApp extends React.Component {
         axios({
             url: '/xhr/delete/' + id,
             method: 'DELETE',
-            headers: {'X-Requested-With': 'XMLHttpRequest'}
-        }).then((res) => {
+            headers: { 'X-Requested-With': 'XMLHttpRequest' }
+        }).then(res => {
             if (res.data.status) {
                 this.setState({ data: remainder });
             }
-        })
+        });
     }
 
     render() {
-        return (
-            <div>
-                <TodoForm addTodo={this.addTodo.bind(this)} />
-                <TodoList todos={this.state.data} remove={this.deleteTodo.bind(this)} />
-            </div>
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(TodoForm, { addTodo: this.addTodo.bind(this) }),
+            React.createElement(TodoList, { todos: this.state.data, remove: this.deleteTodo.bind(this) })
         );
     }
 }
 
-ReactDOM.render(<TodoApp />, document.getElementById('todo'));
+ReactDOM.render(React.createElement(TodoApp, null), document.getElementById('todo'));
+//# sourceMappingURL=todolist.js.map
